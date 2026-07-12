44b5579ce26a80eb727baa7e94e31f02d17c55ed
Manwani Khushi <manvanikhushi9@gmail.com>
2026-07-12 09:44:02 +0530

    Initial project setup

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+
+$user = "root";
+
+$password = "";
+
+$database = "transitops";
+
+$conn = mysqli_connect($host,$user,$password,$database);
+
+if(!$conn){
+
+die("Database Connection Failed");
+
+}
+
+?>
